694cec08e66c0ecb7f3256c74a82f15a42dc2021
DaveXo9 <85836822+DaveXo9@users.noreply.github.com>
2023-01-06 21:16:59 +0100

    Add files via upload
    Napravljen GUI  sa prikazom plota u samo aplikaciji

--- projekt.R
+++ projekt.R
@@ -1,40 +1,76 @@
-rm(list=ls())
+rm(list = ls())
+library(shiny)
+library(shinythemes)
 library(dplyr)
 library(ggplot2)
 
 #url <- 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 #destfile <- 'C:\\Users\\User\\Documents\\Fakultet\\Usluge zasnovane na lokaciji\\owid-covid-data.csv'
 #download.file(url,destfile)
-data <- as.data.frame(read.csv('C:\\Users\\User\\Documents\\Fakultet\\Usluge zasnovane na lokaciji\\owid-covid-data.csv', header=TRUE, sep = ','))
-chosenDay<- filter(data, date == "2020-10-21")
-dayBefore <- filter(data, date=="2020-10-20")
+data <-
+  as.data.frame(
+    read.csv(
+      'C:\\Users\\User\\Documents\\Fakultet\\Usluge zasnovane na lokaciji\\owid-covid-data.csv',
+      header = TRUE,
+      sep = ','
+    )
+  )
 
-chosenDay[is.na(chosenDay)] = 0
-chosenDay$location[chosenDay$location=="United States"] = "USA"
-chosenDay$location[chosenDay$location=="United Kingdom"] = "UK"
 
-dayBefore[is.na(dayBefore)] = 0
-dayBefore$location[dayBefore$location=="United States"] = "USA"
-dayBefore$location[dayBefore$location=="United Kingdom"] = "UK"
+ui <- fluidPage(
+  theme = shinytheme("lumen"),
+  titlePanel("Covid 19 data"),
+  sidebarLayout(
+    sidebarPanel(
+      # Select date range to be plotted
+      dateRangeInput(
+        "date",
+        strong("Date range"),
+        start = "2020-01-01",
+        end = "2022-12-31",
+        min = "2020-01-01",
+        max = "2022-12-31"
+      ),
+    ),
+    
+    # Output: Description, lineplot, and reference
+    mainPanel(
+      plotOutput(outputId = "lineplot", height = "500px"),
+    )
+  )
+)
+server <- function(input, output) {
+    
+    output$lineplot <- renderPlot({ 
+      validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
+      validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
+      chosenDay <- filter(data, date == input$date[2])
+      dayBefore <- filter(data, date == input$date[1])
+      
+      chosenDay[is.na(chosenDay)] = 0
+      chosenDay$location[chosenDay$location == "United States"] = "USA"
+      chosenDay$location[chosenDay$location == "United Kingdom"] = "UK"
+      
+      dayBefore[is.na(dayBefore)] = 0
+      dayBefore$location[dayBefore$location == "United States"] = "USA"
+      dayBefore$location[dayBefore$location == "United Kingdom"] = "UK"
+      
+      casesDifference = (chosenDay$new_cases - dayBefore$new_cases)
+      
+      world <- map_data("world")
+      
+      world$variable <- casesDifference[match(world$region, chosenDay$location)]
+      
+      world_map <- ggplot(world, aes(x = long, y = lat)) +
+        geom_polygon(aes(group = group, fill = variable),
+                     col = "white",
+                     lwd = 0.1)
+      world_map + scale_fill_gradientn(
+        colors = c("#0093ff", "#c0e4ff", "#ffa6a6", "#ff0000"),
+        values = scales::rescale(c(-2000, -0.1, 0.1, 6000))
+      )
+      })
+}
 
-casesDifference =(chosenDay$new_cases - dayBefore$new_cases) 
-
-world <- map_data("world")
-
-world$variable <- casesDifference[match(world$region,chosenDay$location)]
-breaks = c(0)
-
-#newWorld <- world[order(world[['variable']]), ] 
-#print(newWorld)
-#print(world)
-
-world_map <- ggplot(world, aes(x=long, y=lat)) + 
-  geom_polygon(aes(group=group, fill=variable), col="white",lwd=0.1)
-#world_map + scale_fill_gradient(low='darkblue', high='red')
-
-#Moguće rješenje za problem sa bojama
-# colors prima niz boja 
-# scales::rescale prima brojeve koji označavaju intervale za pojedinu boju
-world_map + scale_fill_gradientn(colors = c("#0093ff","#c0e4ff", "#ffa6a6", "#ff0000"),
-      values =scales::rescale(c(-2000,-0.1, 0.1,6000)))
+shinyApp(ui = ui, server = server)
 
